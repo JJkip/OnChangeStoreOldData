1dcde0e2774a86de6f80f6b10f870d55772c2e9a
jjkip <jjlangatjose@gmail.com>
2023-05-23 23:47:33 +0300

    Initial Commit

---
+++ OnChangeStoreOldData/OnChangeStoreOldDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnChangeStoreOldDataApp.swift
+//  OnChangeStoreOldData
+//
+//  Created by Joseph Langat on 23/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct OnChangeStoreOldDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
